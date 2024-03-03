7c10e3e15a671aefb80e3605d761adf998f0b0e9
Ricardo Valladares Navarro <107517375+Ricarditoloco@users.noreply.github.com>
2024-03-03 00:02:42 -0600

    Update LLM LOCAL.py

--- LLM LOCAL.py
+++ LLM LOCAL.py
@@ -5,13 +5,13 @@ from transformers import AutoTokenizer, AutoModelForCausalLM
 tokenizer = AutoTokenizer.from_pretrained("google/gemma-2b-it")
 model = AutoModelForCausalLM.from_pretrained("google/gemma-2b-it")
 
-# Función para generar el poema
+# Función para generar el texto
 def generate_poem():
     input_text = text_entry.get()  # Obtener el texto de la entrada
     input_ids = tokenizer(input_text, return_tensors="pt")
     outputs = model.generate(**input_ids)
     poem = tokenizer.decode(outputs[0])
-    output_label.config(text=poem)  # Mostrar el poema en la etiqueta de salida
+    output_label.config(text=poem)  # Mostrar el texto en la etiqueta de salida
 
 # Crear la ventana principal
 root = tk.Tk()
@@ -21,11 +21,11 @@ root.title("Chat Gpt Local")
 text_entry = tk.Entry(root, width=50)
 text_entry.pack(padx=20, pady=10)  # Añadir espacio en los costados y en la parte superior e inferior
 
-# Crear el botón de generación de poema
+# Crear el botón de generación de texto
 generate_button = tk.Button(root, text="Generar Respuesta", command=generate_poem)
 generate_button.pack(pady=5)
 
-# Crear la etiqueta para mostrar el poema generado
+# Crear la etiqueta para mostrar el texto generado
 output_label = tk.Label(root, text="", wraplength=400, justify="center")
 output_label.pack(pady=10)
 
